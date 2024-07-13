1fc087f1e27a9971cf36d373de358f3557e1c925
Vinícius Henrique de Oliveira <63620777+Vini28498@users.noreply.github.com>
2024-07-13 13:51:56 -0300

    Update procs.sql

--- dev/procs.sql
+++ dev/procs.sql
@@ -30,12 +30,12 @@ begin
 	insert into seed.dw.cliente
 	with tb_cliente_init as (
 	select	cargatimestamp as dataAtualizacao,
-			clientecodigo as id,
-			clientedescricao as nomeFantasia,
-			clientecpfcnpj as documento,
-			clientecelular as celular,
-			clientetelefone as telefoneFixo,
-			clienteemail as email
+		clientecodigo as id,
+		clientedescricao as nomeFantasia,
+		clientecpfcnpj as documento,
+		clientecelular as celular,
+		clientetelefone as telefoneFixo,
+		clienteemail as email
 	from data_raw."1250".cliente
 	where clientecodigo is not null
 	), tb_cnpj as (
@@ -84,28 +84,28 @@ begin
 	insert into seed.dw.produto
 	with tb_produto_init as (
 	select  produtocodigo as id,
-			p.cargatimestamp as dataAtualizacao,
-			cast(null as varchar(254)) as sku,
-			produtodescricao as descricao,
-			fabricantedescricao as branding,
-			case 
-				when produtodescricao like '%LT%'
-					then 'LT'
-				when produtodescricao like '%KG%'
-					then 'KG'
-				when produtodescricao like '% L%'
-					then 'LT'
-				else produtounidademedida
-			end as um,
-			produtosubgrupo as segmento,
-			REGEXP_SUBSTR(REGEXP_SUBSTR(produtodescricao, '[(](.*)[)]'), '[0-9]+') as valorUnidadeMedida,
-			produtofatorconversao as fatorConversaoQuantidade
+		p.cargatimestamp as dataAtualizacao,
+		cast(null as varchar(254)) as sku,
+		produtodescricao as descricao,
+		fabricantedescricao as branding,
+		case 
+			when produtodescricao like '%LT%'
+				then 'LT'
+			when produtodescricao like '%KG%'
+				then 'KG'
+			when produtodescricao like '% L%'
+				then 'LT'
+			else produtounidademedida
+		end as um,
+		produtosubgrupo as segmento,
+		REGEXP_SUBSTR(REGEXP_SUBSTR(produtodescricao, '[(](.*)[)]'), '[0-9]+') as valorUnidadeMedida,
+		produtofatorconversao as fatorConversaoQuantidade
 	from data_raw."1250".produto as p
 	inner join data_raw."1250".fabricante as ff
 	on
 		ff.fabricantecodigo = p.produtofabricante
 	where   (fabricantedescricao like '%manufacturer1%' or
-			fabricantedescricao like '%manufacturer2%' or
+		fabricantedescricao like '%manufacturer2%' or
 	    	fabricantedescricao like '%manufacturer3%') and
 	    	produtocodigo is not null
 	), tb_cnpj as (
@@ -156,40 +156,40 @@ begin
 	insert into seed.dw.faturamento
 	with tb_faturamento_init as (
 	select	row_number() over (order by faturamentonumeronf) as id,
-			f.cargatimestamp as dataAtualizacao,
-			faturamentocliente as idCliente,
-			--documento do cliente tem que fazer relacionamento
-			faturamentonumeronf as numeroNotaFiscalFaturamento,
-			faturamentoserienf as serieNotaFiscalFaturamento,
-			faturamentocfop as cfopFaturamento,
-			cast(faturamentodata as date) as dataEmissaoFaturamento,
-			faturamentocancelado as statusNotaFiscalFaturamento,
-			cast(faturamentovalorbrutoitem as double precision) as valorTotalProdutosFaturamento,
-			cast(faturamentovaloritem as double precision) as valorTotalNfFaturamento,
-			faturamentosituacao as tipoFaturamento
+		f.cargatimestamp as dataAtualizacao,
+		faturamentocliente as idCliente,
+		--documento do cliente tem que fazer relacionamento
+		faturamentonumeronf as numeroNotaFiscalFaturamento,
+		faturamentoserienf as serieNotaFiscalFaturamento,
+		faturamentocfop as cfopFaturamento,
+		cast(faturamentodata as date) as dataEmissaoFaturamento,
+		faturamentocancelado as statusNotaFiscalFaturamento,
+		cast(faturamentovalorbrutoitem as double precision) as valorTotalProdutosFaturamento,
+		cast(faturamentovaloritem as double precision) as valorTotalNfFaturamento,
+		faturamentosituacao as tipoFaturamento
 	from data_raw."1250".faturamento as f
 	inner join data_raw."1250".fabricante as ff
 	on
 		ff.fabricantecodigo = f.faturamentofabricante
 	where   (fabricantedescricao like '%manufacturer1%' or
-			fabricantedescricao like '%manufacturer2%' or
+		fabricantedescricao like '%manufacturer2%' or
 	    	fabricantedescricao like '%manufacturer3%') and
 	    	faturamentoano in ('2024')
 	), tb_faturamento_final as (
 		select 	fi.id,
-				fi.dataAtualizacao,
-				fi.idCliente,
-				fi.numeroNotaFiscalFaturamento,
-				fi.serieNotaFiscalFaturamento,
-				fi.cfopFaturamento,
-				fi.dataEmissaoFaturamento,
-				fi.statusNotaFiscalFaturamento,
-				fi.valorTotalProdutosFaturamento,
-				fi.valorTotalNfFaturamento,
-				fi.tipoFaturamento,
-				c.documento as documentoCliente,
-				f.filialcnpj as cnpjOrigemDados,
-				p.valorUnidadeMedida
+			fi.dataAtualizacao,
+			fi.idCliente,
+			fi.numeroNotaFiscalFaturamento,
+			fi.serieNotaFiscalFaturamento,
+			fi.cfopFaturamento,
+			fi.dataEmissaoFaturamento,
+			fi.statusNotaFiscalFaturamento,
+			fi.valorTotalProdutosFaturamento,
+			fi.valorTotalNfFaturamento,
+			fi.tipoFaturamento,
+			c.documento as documentoCliente,
+			f.filialcnpj as cnpjOrigemDados,
+			p.valorUnidadeMedida
 		from tb_faturamento_init as fi
 		left join seed.dw.cliente as c
 		on
@@ -252,66 +252,66 @@ begin
 	insert into seed.dw.faturamento_item
 	with tb_faturamento_init as (
 	select	row_number() over (order by faturamentonumeronf) as id,
-			f.cargatimestamp as dataAtualizacao,
-			faturamentonumeronf as numero,
-			faturamentoserienf as serie,
-			cast(faturamentodata as date) as dataEmissao,
-			case 
-				when faturamentocfop > 5000
-					then 1
-				else 0
-			end as tipo,
-			null ::varchar idNotaFiscalOrigem,
-			null ::varchar notaFiscalOrigem,
-			faturamentoproduto as idItem,
-			-- descricao do produto tem que fazer relacionamento
-			null ::varchar as sku,
-			-- unidade de medida tem que fazer relacionamento
-			null ::varchar loteItem,
-			cast(faturamentovolumeitem as double precision) as quantidadeItem,
-			cast(faturamentovalorbrutoitem as double precision) as faturamentovalorbrutoitem,
-			cast(faturamentovolumeitem as double precision) as faturamentovolumeitem,
-			cast(faturamentovalorbrutoitem as double precision) as valorTotalItem,
-			faturamentocfop as cfopItem,
-			-- cpf fo vendedor tem que fazer relacionamento
-			faturamentovendedor,
-			faturamentofilial
+		f.cargatimestamp as dataAtualizacao,
+		faturamentonumeronf as numero,
+		faturamentoserienf as serie,
+		cast(faturamentodata as date) as dataEmissao,
+		case 
+			when faturamentocfop > 5000
+				then 1
+			else 0
+		end as tipo,
+		null ::varchar idNotaFiscalOrigem,
+		null ::varchar notaFiscalOrigem,
+		faturamentoproduto as idItem,
+		-- descricao do produto tem que fazer relacionamento
+		null ::varchar as sku,
+		-- unidade de medida tem que fazer relacionamento
+		null ::varchar loteItem,
+		cast(faturamentovolumeitem as double precision) as quantidadeItem,
+		cast(faturamentovalorbrutoitem as double precision) as faturamentovalorbrutoitem,
+		cast(faturamentovolumeitem as double precision) as faturamentovolumeitem,
+		cast(faturamentovalorbrutoitem as double precision) as valorTotalItem,
+		faturamentocfop as cfopItem,
+		-- cpf fo vendedor tem que fazer relacionamento
+		faturamentovendedor,
+		faturamentofilial
 	from data_raw."1250".faturamento as f
 	inner join data_raw."1250".fabricante as ff
 	on
 		ff.fabricantecodigo = f.faturamentofabricante
 	where   (fabricantedescricao like '%manufacturer1%' or
-			fabricantedescricao like '%manufacturer2%' or
+		fabricantedescricao like '%manufacturer2%' or
 	    	fabricantedescricao like '%manufacturer3%') and
 	    	faturamentoano in ('2024')
 	), tb_faturamento_final as (
 		select 	fi.id,
-				fi.dataAtualizacao,
-				fi.numero,
-				fi.serie,
-				fi.dataEmissao,
-				fi.tipo,
-				case
-					when tipo = 0
-						then fi.id
-					else null
-				end as idNotaFiscalOrigem,
-				case
-					when tipo = 0
-						then fi.numeroNotaFiscalFaturamento
-					else null
-				end as notaFiscalOrigem,
-				fi.idItem,
-				p.descricao as descricaoItem,
-				fi.sku,
-				fi.loteItem,
-				fi.quantidadeItem,
-				(fi.faturamentovalorbrutoitem)/(fi.faturamentovolumeitem) as valorUnitarioItem,
-				fi.valorTotalItem,
-				fi.cfopItem,
-				v.vendedorcpf as cpfVendedor,
-				p.um as unidadeMedidaItem,
-				p.sku as skuItem
+			fi.dataAtualizacao,
+			fi.numero,
+			fi.serie,
+			fi.dataEmissao,
+			fi.tipo,
+			case
+				when tipo = 0
+					then fi.id
+				else null
+			end as idNotaFiscalOrigem,
+			case
+				when tipo = 0
+					then fi.numeroNotaFiscalFaturamento
+				else null
+			end as notaFiscalOrigem,
+			fi.idItem,
+			p.descricao as descricaoItem,
+			fi.sku,
+			fi.loteItem,
+			fi.quantidadeItem,
+			(fi.faturamentovalorbrutoitem)/(fi.faturamentovolumeitem) as valorUnitarioItem,
+			fi.valorTotalItem,
+			fi.cfopItem,
+			v.vendedorcpf as cpfVendedor,
+			p.um as unidadeMedidaItem,
+			p.sku as skuItem
 		from tb_faturamento_init as fi
 		left join seed.dw.cliente as c
 		on
@@ -381,28 +381,28 @@ begin
 	insert into seed.dw.estoque
 	with tb_estoque_init as (
 	select  row_number() over (order by estoquedata) as id,
-			cast(estoquedata as date) as dataCadastro,
-			e.cargatimestamp as dataAtualizacao,
-			estoqueproduto as idItem,
-			--relacionar com produto
-			cast(estoquesaldo as double precision) as quantidadeDisponivel,
-			estoquefilial
+		cast(estoquedata as date) as dataCadastro,
+		e.cargatimestamp as dataAtualizacao,
+		estoqueproduto as idItem,
+		--relacionar com produto
+		cast(estoquesaldo as double precision) as quantidadeDisponivel,
+		estoquefilial
 	from data_raw."1250".estoque as e
 	inner join data_raw."1250".fabricante as ff
 	on
 		ff.fabricantecodigo = e.estoquefabricante
 	where   (fabricantedescricao like '%manufacturer1%' or
-			fabricantedescricao like '%manufacturer2%' or
+		fabricantedescricao like '%manufacturer2%' or
 	    	fabricantedescricao like '%manufacturer3%') and
 	    	estoqueano in ('2024')
 	), tb_estoque_final as (
 		select	f.filialcnpj as cnpjOrigemDados,
-				ei.id,
-				ei.dataCadastro,
-				ei.dataAtualizacao,
-				ei.idItem,
-				ei.quantidadeDisponivel,
-				p.descricao
+			ei.id,
+			ei.dataCadastro,
+			ei.dataAtualizacao,
+			ei.idItem,
+			ei.quantidadeDisponivel,
+			p.descricao
 		from tb_estoque_init as ei
 		left join seed.dw.produto as p
 		on
